5e3b763f7095aba7f4bcace71e1993c8244608f9
Xyter246 <111178017+Xyter246@users.noreply.github.com>
2023-03-24 00:03:37 +0100

    Camera Edits
    Camera complete:
    - Follow player
    - Zoom in and out, with a maximum and minimum

---
+++ AutoHouse/Assets/Scripts/CameraZoomScrollWheel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomScrollWheel : MonoBehaviour
+{
+    [SerializeField] private float ScrollSpeed = 10f;
+    [SerializeField] private float minZoom = 15f;
+    [SerializeField] private float maxZoom = 50f;
+    private Camera ZoomCamera;
+
+    private void Start() {
+        ZoomCamera = Camera.main;
+    }
+    
+    private void Update(){
+        // Let player zoom in and out
+        if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+            if (ZoomCamera.orthographicSize >= minZoom && ZoomCamera.orthographicSize <= maxZoom) {
+                ZoomCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+            }
+        }
+        // Set a maximum and minimum zoom distance
+        if (ZoomCamera.orthographicSize < minZoom) {
+            ZoomCamera.orthographicSize = minZoom;
+        } 
+        if (ZoomCamera.orthographicSize > maxZoom) {
+            ZoomCamera.orthographicSize = maxZoom;
+        }
+    }
+}
