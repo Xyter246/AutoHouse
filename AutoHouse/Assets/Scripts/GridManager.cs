58a32f751bf3bc867f3f939bc1b85ae57febbd1d
Xyter246 <111178017+Xyter246@users.noreply.github.com>
2023-04-05 16:03:43 +0200

    16x9 Tilemap
    Custom Tilemap met sprites ipv

---
+++ AutoHouse/Assets/Scripts/GridManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Transform _player;
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++) {
+            for (int y = 0; y < _height; y++) {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+            }
+        }
+
+        _player.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+}
