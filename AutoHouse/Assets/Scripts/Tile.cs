c1c9304d37ac1ef1cd4901885a608bab445c70c4
Xyter246 <111178017+Xyter246@users.noreply.github.com>
2023-04-05 17:52:37 +0200

    gein

--- AutoHouse/Assets/Scripts/Tile.cs
+++ AutoHouse/Assets/Scripts/Tile.cs
@@ -7,6 +7,7 @@ public class Tile : MonoBehaviour
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
+    GridManager gridManager = new GridManager();
 
     public void Init(bool isOffset) {
         _renderer.color = isOffset ? _offsetColor : _baseColor;
@@ -14,7 +15,6 @@ public class Tile : MonoBehaviour
 
     private void OnMouseDown()
     {
-        GridManager gridManager = new GridManager();
         Vector2 mousePosition = gridManager.GetMousePosition();
         Debug.Log("X: " + mousePosition.x + ", Y: " + mousePosition.y);
         gridManager.GetTileAtPosition(mousePosition);
